7a0d123324846c825f48b624861ea0529d543622
Jamie Degnan <jamie.degnan@gmail.com>
2017-08-23 09:48:06 -0700

    Added fade in javascript to page

---
+++ script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal();
+sr.reveal('.introduction');
+sr.reveal('.getting-there');
+sr.reveal('.table')
+sr.reveal('.where-to-camp');
+sr.reveal('.activities');
+sr.reveal('.column-split');
